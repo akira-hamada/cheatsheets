9256091671ae72653e8e926e29676692ed3645a7
akira hamada <drumcorps.enthusiast@gmail.com>
2014-03-25 12:59:30 +0900

    カーソル移動コマンドを追加

--- vim_cheat.rb
+++ vim_cheat.rb
@@ -9,12 +9,34 @@ cheatsheet do
   source_url 'https://github.com/akira-hamada/public_cheatsheet/blob/master/vim_cheat.rb'
 
   category do
+    id 'カーソル移動'
+
+    entry do
+      name '移動前の位置に戻る'
+      notes <<-'CODE'
+        ```vim
+          <C-o>
+        ```
+      CODE
+    end
+
+    entry do
+      name '\<C-o\>の逆(動かない)'
+      notes <<-'CODE'
+        ```vim
+          <C-i>
+        ```
+      CODE
+    end
+  end
+
+  category do
     id '選択系'
 
     entry do
       name '直前に選択していた範囲を再選択'
       notes <<-'CODE'
-        ```vimscript
+        ```vim
           gv
         ```
       CODE
